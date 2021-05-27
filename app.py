9e3d04a4505739d98cb5bb6dc43e45974d85ae4a
yusuf963 <mohammad963yusuf@gmail.com>
2021-05-28 00:19:48 +0100

    resolving the lib error

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from speedtest import Speedtest
 
 st = Speedtest()
-print('your connection download speed is', st.download())
-print('your connection upload speed is', st.upload())
+print('your connection download speed is', st.download()/1000000)
+print('your connection upload speed is', st.upload()/1000000)
